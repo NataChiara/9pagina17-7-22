2e1f8a297fbde62ff9b0ef1f664a47dcb34d21cc
NataChiara <natachiara@outlook.com>
2022-07-19 23:22:24 -0300

    dark/light switch in Link component

--- src/pages/Link.jsx
+++ src/pages/Link.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, {useState, useContext} from "react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 import './Pages.css';
+import ThemeContext from "../components/Context/ThemeContext";
 
 const Link =() => {
+    const [darkMode, setDarkMode] = useState(true);
+
+    const Theme = useContext (ThemeContext);
+    
     return (
         <React.Fragment>
             <Header/>
-            <div className="body">
+            <div className={darkMode? "main-dark" : "main-light"} style={{Theme}}>
+                <button type="button" onClick={()=> {setDarkMode(!darkMode)}}>{darkMode? "Dark Mode ON" : "Light Mode ON"} </button>
+                <br/><br/>
                 <h1>Food</h1>
+                <br/>
                 <p>Cows like to eat grass</p>
             </div>
             <Footer/>
